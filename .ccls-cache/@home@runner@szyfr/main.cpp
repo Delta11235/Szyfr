098a6dc9d890bb2f23c404c6d5dd9edc56d4d5ff
Delta <114833391+Delta11235@users.noreply.github.com>
2023-03-20 21:47:16 +0000

    Pokazuje ascii

--- .ccls-cache/@home@runner@szyfr/main.cpp
+++ .ccls-cache/@home@runner@szyfr/main.cpp
@@ -5,10 +5,14 @@ using namespace std;
 
 int main() {
   ifstream plik("tekst.txt");
-  char haslo[35];
+  char haslo[50];
   while (plik >> haslo) {
     int dlugosc = string(haslo).length();
     cout << haslo << endl << dlugosc << endl;
+    cout << "Ascii: " << endl;
+    for (int a = 0; a < dlugosc; a++) {
+      cout << (int)haslo[a] << " " << haslo[a] << endl;
+    }
   }
   plik.close();
   return 0;
